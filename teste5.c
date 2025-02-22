d97b3ba8e99aecc422a652f2f9b62d47def3af35
AlunoSabrina <alunoestaciosabrina@gmail.com>
2025-02-22 01:08:17 +0000

    fiz a carta 1

--- teste5.c
+++ teste5.c
@@ -2,37 +2,40 @@
  
 int main (){
 
-int Codigo_1;
-char Cidade_1[20];
-char Estado_1[2] = A;
-int Populacao_1;
-float Area_1;
-float PIB_1;
-int Pontos_Turisticos_1;
+
+
+char Estado_1[5];
+char codigo_1[20], Cidade_1[20];
+int Populacao_1 , Pontos_Turisticos_1;
+float Area_1, PIB_1;
+
+//cadastro das cartas
+printf ("carta 1: \n");
 
 printf ("Digite o Codigo: \n");
-scanf ("%d", &Codigo_1);
-
-printf ("Digite a Cidade: \n");
-scanf ("%s", &Cidade_1);
-
+scanf (" %s", &codigo_1);
 printf ("Digite o Estado: \n");
-scanf ("%s", &Estado_1);
-
+scanf (" %s", &Estado_1);
+printf ("Digite a Cidade: \n");
+scanf (" %s", &Cidade_1);
 printf ("Digite a Populacao: \n");
-scanf ("%d", &Populacao_1);
-
+scanf (" %d", &Populacao_1);
 printf ("Digite a Area: \n");
-scanf ("%f", &Area_1);
-
+scanf (" %f", &Area_1);
 printf ("Digite o PIB: \n");
-scanf ("%f", &PIB_1);
-
+scanf (" %f", &PIB_1);
 printf ("Digite o numero de Pontos Turisticos:");
-scanf ("%d", &Pontos_Turisticos_1);
+scanf (" %d", &Pontos_Turisticos_1);
+
+printf("O Codigo é: %c %s\n", Estado_1, codigo_1);
+printf ("A cidade é: %s\n", Cidade_1);
+printf ("A população é: %d\n", Populacao_1);
+printf ("A aréa é: %f\n", Area_1);
+printf ("O PIB é: %f\n", PIB_1);
+printf("Quantidade de pontos turisticos é: %d\n", Pontos_Turisticos_1);
+
 
 
 return 0;
-
 
 }
